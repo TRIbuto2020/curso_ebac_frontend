c68099112e5ab475423df7acc5add70d3c8f825a
Vitor RS <vitor2000rs@gmail.com>
2023-02-06 10:21:34 -0300

    commit exercicio

---
+++ src/exercicio.js
@@ -0,0 +1,10 @@
+"use strict";
+function multiplica(a, b) {
+    return a * b;
+}
+;
+function dizOla(nome) {
+    return `Olá, ${nome}`;
+}
+console.log(multiplica(15, 48));
+console.log(dizOla("Vitor"));
---
+++ src/exercicio.ts
@@ -0,0 +1,10 @@
+function multiplica(a: number, b: number): number {
+    return a * b
+};
+
+function dizOla(nome: string): string {
+    return `Olá, ${nome}`
+} 
+
+console.log(multiplica(15, 48));
+console.log(dizOla("Vitor"));
